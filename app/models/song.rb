6ff6cb3beffd01aa580569b51648415f12baf988
Abdul Redd <104014874+abdulredd@users.noreply.github.com>
2024-09-08 20:53:12 -0400

    Update song.rb

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,17 +1,16 @@
 class Song < ApplicationRecord
   belongs_to :artist
 
-# describe '#artist' do
-#   def artist
-#     Artist.find(self.artist_id)
-#   end
+  # # artist method without associations
+  # def artist
+  #   Artist.find(self.artist_id)
+  # end
 
-  # describe '#artist_name' do
   def artist_name
     self.artist.name
+    # note: self is optional. This solution could also be `artist.name`
   end
 
-  # describe '#other_artist_songs' do
   def other_artist_songs
     self.artist.songs.where.not(id: self.id)
   end
