3caab6c520a4f683b23be6d4c8fa5a884b119a77
Juliet Eyraud <juliet@turing.edu>
2024-11-29 21:51:17 -0500

    unsplash complete

--- app/controllers/api/v1/images_controller.rb
+++ app/controllers/api/v1/images_controller.rb
@@ -1,5 +1,24 @@
 class Api::V1::ImagesController < ApplicationController
   def show
-    render json: { data: {} }
+    artist = params[:artist]
+    conn = Faraday.new(url: "https://api.unsplash.com") 
+    
+    response = conn.get("/search/photos", { query: artist, client_id: Rails.application.credentials.unsplash[:key] })
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    first_photo = json[:results][0]
+
+    formatted_json = {
+      id: nil,
+      type: "image",
+      attributes: {
+        image_url: first_photo[:urls][:raw],
+        photographer: first_photo[:user][:name],
+        photographer_url: first_photo[:user][:links][:self],
+        alt_text: first_photo[:alt_description]
+      }
+    }
+
+    render json: { data: formatted_json }
   end
 end
